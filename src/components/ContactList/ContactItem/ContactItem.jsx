dcd3a1e7756990ace6d1453c8656d4159b001a77
Dima Telechkun <telechkun93@gmail.com>
2023-01-27 12:46:07 +0200

    loader component was applied

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,17 +1,23 @@
+import Loader from 'components/Loader/Loader';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteContactRequest } from 'redux/thunks/contactsThunk';
 import css from './ContactItem.module.css';
 
 export const ContactItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
+
+  const isLoading = useSelector(state => state.contacts.isLoading);
+
   return (
     <li className={css.namesItem}>
       {name}
+      {isLoading && <Loader />}
       <span className={css.colon}> : </span>
       <span className={css.numTel}>{number}</span>
       <button
         className={css.btn}
+        disabled={isLoading}
         type="button"
         onClick={() => dispatch(deleteContactRequest(id))}
       >
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,11 +2,11 @@ import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 function ContactsPage() {
-  const isLoading = useSelector(state => state.contacts.isLoading);
-  console.log(isLoading);
+  // const isLoading = useSelector(state => state.contacts.isLoading);
+  // console.log(isLoading);
   return (
     <div>
       <h1>Phonebook</h1>
